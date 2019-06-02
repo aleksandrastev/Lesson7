dc7e37af1cb1f7ead4d073e262603fd697d56b5c
aleksandrastev <leafsoul91@gmail.com>
2019-06-02 16:51:11 +0200

    SuperArrayListTest for testing the pop() method

---
+++ test/SuperArrayListTest.java
@@ -0,0 +1,26 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import exercises.SuperArrayList;
+
+class SuperArrayListTest {
+
+	@Test
+	void testPop() {
+		SuperArrayList list = new SuperArrayList();
+		list.add(1);
+		list.add("blue");
+		list.add('c');
+		list.add(true);
+		list.add(7);
+		list.pop();
+		assertEquals(4, list.size());
+		list.add(5);
+		list.add(6);
+		list.pop();
+		assertEquals(5, list.size());
+
+	}
+
+}
